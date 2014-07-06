db99a9ee2441d5821ea3413113859ebb12257380
Ger Hobbelt <ger@hobbelt.com>
2014-07-06 18:53:59 +0200

    half-hearted attempt to upgrade the test specs to Jasmine 2.0

--- spec/api/generated-parser-behavior.spec.js
+++ spec/api/generated-parser-behavior.spec.js
@@ -23,103 +23,12 @@ describe("generated parser behavior", function() {
     for (i = 0; i < optionsVariants.length; i++) {
       describe(
         "with options " + jasmine.pp(optionsVariants[i]),
-        function() { block(clone(optionsVariants[i])); }
+        function() { 
+          block(clone(optionsVariants[i])); 
+        }
       );
     }
   }
-
-  beforeEach(function() {
-    this.addMatchers({
-      toParse: function(input) {
-        var options  = arguments.length > 2 ? arguments[1] : {},
-            expected = arguments[arguments.length - 1],
-            result;
-
-        try {
-          result = this.actual.parse(input, options);
-
-          this.message = function() {
-            return "Expected " + jasmine.pp(input) + " "
-                 + "with options " + jasmine.pp(options) + " "
-                 + (this.isNot ? "not " : "")
-                 + "to parse as " + jasmine.pp(expected) + ", "
-                 + "but it parsed as " + jasmine.pp(result) + ".";
-          };
-
-          return this.env.equals_(result, expected);
-        } catch (e) {
-          this.message = function() {
-            return "Expected " + jasmine.pp(input) + " "
-                 + "with options " + jasmine.pp(options) + " "
-                 + "to parse as " + jasmine.pp(expected) + ", "
-                 + "but it failed to parse with message "
-                 + jasmine.pp(e.message) + ".";
-          };
-
-          return false;
-        }
-      },
-
-      toFailToParse: function(input) {
-        var options = arguments.length > 2 ? arguments[1] : {},
-            details = arguments.length > 1
-                        ? arguments[arguments.length - 1]
-                        : undefined,
-            result;
-
-        try {
-          result = this.actual.parse(input, options);
-
-          this.message = function() {
-            return "Expected " + jasmine.pp(input) + " "
-                 + "with options " + jasmine.pp(options) + " "
-                 + "to fail to parse"
-                 + (details ? " with details " + jasmine.pp(details) : "") + ", "
-                 + "but it parsed as " + jasmine.pp(result) + ".";
-          };
-
-          return false;
-        } catch (e) {
-          /*
-           * Should be at the top level but then JSHint complains about bad for
-           * in variable.
-           */
-          var key;
-
-          if (this.isNot) {
-            this.message = function() {
-              return "Expected " + jasmine.pp(input)
-                   + "with options " + jasmine.pp(options) + " "
-                   + "to parse, "
-                   + "but it failed with message "
-                   + jasmine.pp(e.message) + ".";
-            };
-          } else {
-            if (details) {
-              for (key in details) {
-                if (details.hasOwnProperty(key)) {
-                  if (!this.env.equals_(e[key], details[key])) {
-                    this.message = function() {
-                      return "Expected " + jasmine.pp(input) + " "
-                           + "with options " + jasmine.pp(options) + " "
-                           + "to fail to parse"
-                           + (details ? " with details " + jasmine.pp(details) : "") + ", "
-                           + "but " + jasmine.pp(key) + " "
-                           + "is " + jasmine.pp(e[key]) + ".";
-                    };
-
-                    return false;
-                  }
-                }
-              }
-            }
-          }
-
-          return true;
-        }
-      }
-    });
-  });
 
   describe("parse", function() {
     var grammar = [
